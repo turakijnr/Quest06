a1bac14ed1ec1c21f7eba989bbb068432264a160
nuuman bashir <bashir_n@qwasar.io>
2020-10-06 10:03:41 +0000

    Quest06

--- ex02/my_data_process.rb
+++ ex02/my_data_process.rb
@@ -1,3 +1,4 @@
+require 'json'
 def my_data_process(csv_processed)
     header = csv_processed[0].split(',')
     body = csv_processed[1..csv_processed.length-1]
@@ -16,5 +17,7 @@ def my_data_process(csv_processed)
         end
         index +=1
     }
-    return hash
+    return hash.to_json
 end 
+
+#puts my_data_process (["Gender,FirstName,LastName,UserName,Email,Age,City,Device,Coffee Quantity,Order At", "Male,Carl,Wilderman,carl,yahoo.com,21->40,Seattle,Safari iPhone,2,afternoon", "Male,Marvin,Lind,marvin,hotmail.com,66->99,Detroit,Chrome Android,2,afternoon", "Female,Shanelle,Marquardt,shanelle,hotmail.com,21->40,Las Vegas,Chrome,1,afternoon", "Female,Lavonne,Romaguera,lavonne,yahoo.com,66->99,Seattle,Chrome,2,morning", "Male,Derick,McLaughlin,derick,hotmail.com,41->65,Chicago,Chrome Android,1,afternoon"])
